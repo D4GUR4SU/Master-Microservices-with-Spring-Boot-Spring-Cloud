bb28d75f758276b6ca34d5764f87ebb07b02bf2c
Dagurasu56 <douglas_search@hotmail.com>
2022-02-09 20:09:06 -0300

    setting up limits-service

---
+++ limits-service/src/main/java/com/dagurasu/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.dagurasu.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dagurasu.microservices.limitsservice.bean.LimitsConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitsConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+}
