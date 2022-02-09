4772ef60a88fe7769b59bcd3e1b0c0a74ee0a3a6
Dagurasu56 <douglas_search@hotmail.com>
2022-02-08 22:26:39 -0300

    creating post entity and many to one relationship with user entity

--- restful-web-services/src/main/java/com/dagurasu/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/dagurasu/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,13 @@
 package com.dagurasu.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -15,16 +17,19 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Size(min = 2, message = "Name should have atleast 2 characters")
 	private String name;
-	
+
 	@Past
 	private Date birthDate;
 
+	@OneToMany(mappedBy = "user")
+	private List<Post> posts;
+
 	protected User() {
 	}
-	
+
 	public User(Integer id, String name, Date birthDate) {
 		super();
 		this.id = id;
@@ -56,9 +61,18 @@ public class User {
 		this.birthDate = birthDate;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", posts=" + posts + "]";
 	}
+
 
 }
