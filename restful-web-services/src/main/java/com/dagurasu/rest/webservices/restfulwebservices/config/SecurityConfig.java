fef43ff60010b31a76c56c6458134dab747e8410
Dagurasu56 <douglas_search@hotmail.com>
2022-02-08 21:55:11 -0300

    create user entity and some test data using h2 database

--- restful-web-services/src/main/java/com/dagurasu/rest/webservices/restfulwebservices/config/SecurityConfig.java
+++ restful-web-services/src/main/java/com/dagurasu/rest/webservices/restfulwebservices/config/SecurityConfig.java
@@ -1,34 +1,19 @@
 package com.dagurasu.rest.webservices.restfulwebservices.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		http.csrf()
-			.disable()
-			.authorizeRequests()
-			.antMatchers("/h2-console/**")
-			.permitAll()
-			.mvcMatchers(HttpMethod.GET, "/users")
-			.permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic();
-		
-		http.headers()
-			.frameOptions()
-			.disable();
-		
-		return http.build();
-	}
+	/*
+	 * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http)
+	 * throws Exception {
+	 * 
+	 * http.csrf() .disable() .authorizeRequests() .antMatchers("/h2-console/**")
+	 * .permitAll() .mvcMatchers(HttpMethod.GET, "/users") .permitAll()
+	 * .anyRequest() .authenticated() .and() .httpBasic();
+	 * 
+	 * http.headers() .frameOptions() .disable();
+	 * 
+	 * return http.build(); }spring.jpa.defer-datasource-initialization=true
+	 */
 	
 }
